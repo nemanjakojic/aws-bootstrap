8b49212c9f4fa9d24da81980709c68504541e95b
nemanjakojic <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-03 23:26:06 -0400

    Automates CloudFormation stack deployment
    Adds
     - main.yml resource specification
     - deploy-infra.sh script that submits IaaC to CloudFormation via AWS CLI

---
+++ deploy/deploy-infra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap
+REGION=us-east-2
+CLI_PROFILE=awsbootstrap
+EC2_INSTANCE_TYPE=t2.micro
+
+echo EC2_INSTANCE_TYPE is $EC2_INSTANCE_TYPE
+echo Region is $REGION
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+
